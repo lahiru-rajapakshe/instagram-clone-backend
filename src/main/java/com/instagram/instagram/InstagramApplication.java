5f885bde40784ec16d672ba52c7d9a71a3f0d80a
lara <lahirurajapakshe.stack@gmail.com>
2022-12-06 17:34:36 +0530

    Implement Hashing password

--- src/main/java/com/instagram/instagram/InstagramApplication.java
+++ src/main/java/com/instagram/instagram/InstagramApplication.java
@@ -2,8 +2,9 @@ package com.instagram.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class InstagramApplication {
 
 	public static void main(String[] args) {
